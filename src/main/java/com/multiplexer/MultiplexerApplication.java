03a6c997121b61bb4483dd5c25020335b1bede76
Prateek Krishna <prateekkrishna09@gmail.com>
2024-01-18 20:00:53 +0530

    springboot init

---
+++ src/main/java/com/multiplexer/MultiplexerApplication.java
@@ -0,0 +1,13 @@
+package com.multiplexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplexerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplexerApplication.class, args);
+    }
+
+}
